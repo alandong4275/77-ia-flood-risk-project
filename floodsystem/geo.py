2a3848cddfee4f510e19c3d9303587dad9474e78
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-08 11:40:39 +0000

    Implemented rivers_with_station

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def rivers_with_station(stations):
+    """Returns a list of the names of rivers with a monitoring station"""
+
+    # Creates a set of rivers with monitoring stations
+    rivers = set()
+    for station in stations:
+        if station.river is not None:
+            rivers.add(station.river)
+
+    # Converts set into alphabetically ordered list
+    sorted_rivers = sorted(rivers)
+
+    return sorted_rivers
