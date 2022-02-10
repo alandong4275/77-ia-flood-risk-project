7b005e24c493659dba27eac0dd4b9db03c7075bb
Ed Brewer <edbrew3r@gmail.com>
2022-02-10 13:02:27 +0000

    Wrote tests for station.py

--- test_station.py
+++ test_station.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
+from sqlalchemy import true
 from floodsystem.station import MonitoringStation
+from floodsystem.station import inconsistent_typical_range_stations
 
 
 def test_create_monitoring_station():
@@ -25,3 +27,40 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_typical_range_consistent():
+
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+
+    trange2 = (2, -1)
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange2, river, town)
+
+    assert s1.typical_range_consistent == True
+    assert s2.typical_range_consistent == False
+
+def test_inconsistent_typical_range_stations():
+
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+
+    trange2 = (2, -1)
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange2, river, town)
+    s = [s1, s2]
+
+    inconsistent_s = inconsistent_typical_range_stations(s)
+    assert len(inconsistent_s) == 0
