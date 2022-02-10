078081b360f5171b0aed3dc1632c0bd5213432dc
Ed Brewer <edbrew3r@gmail.com>
2022-02-10 13:33:35 +0000

    Wrote test for stations_within_radius

--- test_geo.py
+++ test_geo.py
@@ -16,3 +16,14 @@ def test_stations_by_distance():
     for index in range(0, len(sorted_stations)):
         assert haversine(sorted_stations[index].coord, p, unit=Unit.KILOMETERS) < haversine(sorted_stations[index+1].coord, p, unit=Unit.KILOMETERS)
 
+
+def test_stations_within_radius():
+    # Build list of stations
+    stations = build_station_list()
+    r = 10
+    centre = (52.2053, 0.1218)
+    s_w_r = geo.stations_within_radius(stations, centre, r)
+
+    for index in range(0, len(s_w_r)):
+        assert haversine(s_w_r[index], centre) < 10
+
