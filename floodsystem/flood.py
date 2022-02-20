8b1e53ab6c88b8899604bc2c657600bc6e62e458
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-20 22:30:12 +0000

    Fixed flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -6,9 +6,10 @@ def stations_level_over_threshold(stations, tol):
     
     s_over_threshold = []
     for station in stations:
-        if station.typical_range_consistent():
-            if station.relative_water_level() > tol:
-                s_over_threshold.append((station, station.relative_water_level()))
+        if station.latest_level != None:
+            if station.typical_range_consistent():
+                if station.relative_water_level() > tol:
+                    s_over_threshold.append((station, station.relative_water_level()))
 
     sorted_list = sorted_by_key(s_over_threshold, 1)
     return sorted_list
