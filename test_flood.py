142e0447f54e09b30633f923667e3b21f4a90b41
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-21 19:00:08 +0000

    Fixed test

--- test_flood.py
+++ test_flood.py
@@ -1,13 +1,20 @@
+from hashlib import sha1
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
+from floodsystem.station import MonitoringStation
 
 def test_stations_level_over_threshold(stations, tol):
-     # Build list of stations
-    stations = build_station_list()
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
 
-    # Update latest level data for all stations
-    update_water_levels(stations)
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
 
     # Print stations with relative water levels over tolerance
-    if len(stations_level_over_threshold(stations, 0.8)) >= 0:
+    if len(stations_level_over_threshold(s1, 0.8)) >= 0:
         assert True
