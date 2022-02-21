c967b072a34ffb6f6862a3609093208762c8db47
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-21 19:04:06 +0000

    Fixed test_flood

--- test_flood.py
+++ test_flood.py
@@ -3,7 +3,7 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 from floodsystem.station import MonitoringStation
 
-def test_stations_level_over_threshold(stations, tol):
+def test_stations_level_over_threshold():
     # Create a station
     s_id = "test-s-id"
     m_id = "test-m-id"
