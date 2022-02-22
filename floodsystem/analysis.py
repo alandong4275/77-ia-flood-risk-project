0f145c19b0f0bae9c0a9b0ea4afa83451b937a37
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-22 15:06:35 +0000

    Implemented polyfit

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,5 +1,10 @@
 import numpy as np
-import matplotlib
+import matplotlib as plt
 
 def polyfit(dates, levels, p):
-    pass
+    """Returns a tuple of a least-squares fit polynomial object and any shift of the time (date) axis"""
+    x = plt.dates.date2num(dates)
+    y = levels
+    p_coeff = np.polyfit(x - x[0], y, p)
+    poly = np.poly1d(p_coeff)
+    return (poly, -x[0])
