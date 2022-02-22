3bd9fd51ee21a94ba86b86c6f7e67e9bc37298df
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-22 15:13:23 +0000

    Completed Task 2F

---
+++ Task2F.py
@@ -0,0 +1,25 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.plot import plot_water_level_with_fit
+import datetime
+
+def run():
+    """Requirements for Task 2F"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    n_stations = stations_highest_rel_level(stations, 5)
+    
+    for station in n_stations:
+        dt = 2
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_level_with_fit(station, dates, levels, 4)
+
+if __name__ == "__main__":
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
+    run()
