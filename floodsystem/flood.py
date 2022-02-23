0f96601877c79f03229520577081c5f7dceeb228
Ed Brewer <edbrew3r@gmail.com>
2022-02-23 18:58:07 +0000

    task 2C + flood func

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,5 +1,4 @@
-from .utils import sorted_by_key
-
+from utils import sorted_by_key
 
 def stations_level_over_threshold(stations, tol):
     """Returns a list of tuples of stations with latest relative water levels over tol and their relative water levels"""
@@ -13,4 +12,14 @@ def stations_level_over_threshold(stations, tol):
 
     sorted_list = sorted_by_key(s_over_threshold, 1)
     return sorted_list
+
+def stations_highest_rel_level(stations, N):
+    """returns a list of the N stations at which the water level, relative to the typical range, is highest"""
+
+    stationsWithHightWater = []
+    for station in stations:
+        stationsWithHightWater.append((stations.station_id, station.relative_water_level()))
     
+    sortedlist = sorted_by_key(stationsWithHightWater, 1, reverse = True)[:N]
+
+    return sortedlist
