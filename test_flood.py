662d81754958ab47d4d8e79009f9bb3cac6a667a
alandong4275 <71878448+alandong4275@users.noreply.github.com>
2022-02-27 22:26:45 +0000

    Wrote tests for analysis, flood and plot

--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,6 @@
 from hashlib import sha1
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 from floodsystem.station import MonitoringStation
 
 def test_stations_level_over_threshold():
@@ -17,4 +17,19 @@ def test_stations_level_over_threshold():
 
     # Print stations with relative water levels over tolerance
     if len(stations_level_over_threshold([s1], 0.8)) >= 0:
+        assert True
+
+def test_stations_highest_rel_level():
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange1 = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
+
+    # Print stations with relative water levels over tolerance
+    if len(stations_highest_rel_level([s1], 1)) >= 0:
         assert True
---
+++ test_plot.py
@@ -0,0 +1,5 @@
+from floodsystem.plot import plot_water_levels
+from floodsystem.station import MonitoringStation
+
+def test_plot_water_levels():
+    assert True
