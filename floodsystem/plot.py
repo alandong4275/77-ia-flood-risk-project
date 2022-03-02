4b8e71c5b928f7d96f20424f2358926eab665b27
Ed Brewer <edbrew3r@gmail.com>
2022-03-02 12:24:46 +0000

    comments

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -7,16 +7,18 @@ import numpy as np
 
 def plot_water_level_with_fit(station, dates, levels, p):
     """Plots the water level data and the best-fit polynomial"""
+    #plots a polynomial of best fit for the data
     poly, d0 = anal.polyfit(dates, levels, p)
+    #plots data
     x = matplotlib.dates.date2num(dates)
     y = levels
     plt.plot(dates, y)
     x1 = np.linspace(x[0], x[-1], len(dates))
     plt.plot(matplotlib.dates.num2date(x1), poly(x1 - d0))
-
+    #adds red boundary lines for the low and high end of the typical range
     plt.axhline(y=station.typical_range[0], color='r', linestyle='-')
     plt.axhline(y=station.typical_range[1], color='r', linestyle='-')
-
+    #adds labels and rotates the x labels by 45 degrees.
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45)
